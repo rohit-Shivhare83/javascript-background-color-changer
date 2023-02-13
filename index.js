a50a84c64f0fa117f29fd0c7b360aafb2358abf6
Rohit <rohitshivhare8381@gmail.com>
2023-02-13 18:41:09 +0530

    first

---
+++ index.js
@@ -0,0 +1,5 @@
+var switch_color = document.getElementById("switch_color");
+function changeColor() {
+  const color = document.getElementById("color").value;
+  document.body.style.backgroundColor = color;
+}
